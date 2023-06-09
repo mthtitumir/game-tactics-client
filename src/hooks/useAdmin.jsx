fc885dda21fc2d45082d3049f298f8e6cccc7198
mthtitumir <titumirgccc@gmail.com>
2023-06-09 12:14:15 +0600

    useAdmin hook with axios and tanstack query set up coplete

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import useAuth from './useAuth';
+import useAxiosSecure from './useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
 
 const useAdmin = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const {user, loading} = useAuth();
+    const [axiosSecure] = useAxiosSecure();
+    const {data: isAdmin, isLoading: isAdminLoading} = useQuery({
+        queryKey: ['isAdmin', user?.email],
+        enabled: !loading,
+        queryFn: async () =>{
+            const res = await axiosSecure.get(`/users/admin/${user?.email}`);
+            return res.data.admin;
+        }
+    })
+    return [isAdmin, isAdminLoading];
 };
 
 export default useAdmin;
