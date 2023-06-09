b3281dd4e9990709ab518f5ff85f3909746c4703
mthtitumir <titumirgccc@gmail.com>
2023-06-09 13:01:35 +0600

    make admin complete

--- src/Pages/DashBoard/AdminDashboard/Users/Users.jsx
+++ src/Pages/DashBoard/AdminDashboard/Users/Users.jsx
@@ -3,10 +3,27 @@ import useUsers from '../../../../hooks/useUsers';
 import useAuth from '../../../../hooks/useAuth';
 import DashboardHeader from '../../../../componants/DashboardHeader/DashboardHeader';
 import {  FaUsers } from 'react-icons/fa';
+import useAxiosSecure from '../../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const Users = () => {
-    const [users] = useUsers();
+    const [axiosSecure] = useAxiosSecure();
+    const [users, refetch] = useUsers();
     const { user } = useAuth();
+    const handleMakeAdmin = singleUser =>{
+        axiosSecure.patch(`users/admin/${singleUser._id}`)
+        .then(data =>{
+            console.log(data);
+            if(data.data.modifiedCount){
+                refetch();
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: `${singleUser.name} is an Admin Now!`, 
+                  })
+            }
+        })
+    }
     return (
         <div className=''>
             <DashboardHeader heading={"All Users"} />
@@ -30,7 +47,11 @@ const Users = () => {
                                     <th>{index+1}</th>
                                     <td className="">{singleUser.name}</td>
                                     <td>{singleUser.email}</td>
-                                    <td><FaUsers className='text-center text-teal-600 mx-auto text-2xl' /></td>
+                                    <td>
+                                        {
+                                            singleUser.role === 'admin' ? 'admin' : <FaUsers onClick={()=> handleMakeAdmin(singleUser)} className='text-center text-teal-600 mx-auto text-2xl' />
+                                        }
+                                    </td>
                                     <th>
                                         <button className="btn btn-ghost btn-xs">details</button>
                                     </th>
